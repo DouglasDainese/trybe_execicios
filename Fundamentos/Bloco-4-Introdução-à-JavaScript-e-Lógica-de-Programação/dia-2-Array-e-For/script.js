6ad67be3740c5ef747ee09cb6b2b8d0c3590f175
DouglasDainese <douglas_dainese@hotmail.com>
2022-07-21 20:55:29 -0300

    resolvido exercicios 1 - 4

---
+++ Fundamentos/Bloco-4-Introdução-à-JavaScript-e-Lógica-de-Programação/dia-2-Array-e-For/script.js
@@ -0,0 +1,22 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let sum = 0;
+
+for (let num of numbers) {
+    sum = sum + num;
+}
+
+let media =  sum / numbers.length;
+
+console.log (sum);
+console.log (numbers.length);
+console.log (media);
+
+if (media > 20 ) {
+    console.log ("valor maior que 20")
+}
+else {
+    console.log ("valor menor ou igual a 20")
+}
+
+
