809eb65ca3cb7bf76831cc3eabce3e1bd5867600
DouglasDainese <douglas_dainese@hotmail.com>
2022-07-23 17:15:13 -0300

    exercicio 04: descobrir maior numero primo entre 2-50.

---
+++ Fundamentos/Bloco-4-Introdução-à-JavaScript-e-Lógica-de-Programação/dia-3-logica-da-programaçao/Descobrir-maior-numero-primo.js
@@ -0,0 +1,15 @@
+
+let bigNumberPrime = 0;
+
+for (i = 2; i < 50; i +=1) {
+   for (j = 2; j < i; j += 1){
+      if (i % j == 0) {
+         
+      }
+      else{
+         bigNumberPrime = i;
+      }
+     
+   }
+} 
+console.log (bigNumberPrime)
