8f45c34b937282397e5c769d50bc03bcc26a7bcb
DouglasDainese <douglas_dainese@hotmail.com>
2022-07-25 21:20:51 -0300

    Exercicios 6 - Parte 1 objetos e for in

--- Fundamentos/Bloco-4-Introdução-à-JavaScript-e-Lógica-de-Programação/JavaScript-Objetos-e-funções/Parte-1.js
+++ Fundamentos/Bloco-4-Introdução-à-JavaScript-e-Lógica-de-Programação/JavaScript-Objetos-e-funções/Parte-1.js
@@ -3,7 +3,61 @@ let info = {
   origem: 'Pato Donald',
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
-// 1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Valor esperado no console: Bem-vinda, Margarida
+// // 1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Valor esperado no console: Bem-vinda, Margarida
 
-console.log('Bem vinda, ' + info.personagem);
+// // console.log('Bem vinda, ' + info.personagem);
 
+// // 2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. Valor esperado no console: {
+//  // personagem: 'Margarida',
+//  // origem: 'Pato Donald',
+//  // nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//  // recorrente: 'Sim'
+
+// info.recorrente = 'Sim';
+
+// // console.log(info);
+
+// 3 - Faça um for/in que mostre todas as chaves do objeto. Valor esperado no console: 
+// personagem
+// origem
+// nota
+// recorrente
+
+// for (let keys in info) {
+//     console.log(info[keys]);
+// };
+
+
+//5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves.
+
+// let info2 = {
+//     personagem: 'Tio Patinhas',
+//     origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178',
+//     nota: 'O último MacPatinhas',
+//     recorrente: 'Sim',
+//   };
+
+//   for (let keys in info) {
+//     console(info[keys]);
+// };
+
+// 6 - Acesse as chaves nome, sobrenome e titulo, que está dentro da chave livrosFavoritos, e faça um console.log no seguinte formato: "O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'".
+
+
+// 🚀 7 - Adicione um novo livro favorito na chave livrosFavoritos, que é um array. Atribua a esta chave um objeto contendo as seguintes informações:
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+
+
+  console.log ('"O livro favorito de ' + leitor.nome + " " + leitor.sobrenome + ' se chama ' + leitor.livrosFavoritos[0]['titulo']+ '"');
