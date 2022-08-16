5997f6779b5e9623c246549e8dbec0747a163a9b
DouglasDainese <douglas_dainese@hotmail.com>
2022-08-16 20:18:50 -0300

    Exercicio 1 - Desafio 2, 3 concluido

--- Fundamentos/Bloco-7/dia-7.3/jest/sum.test.js
+++ Fundamentos/Bloco-7/dia-7.3/jest/sum.test.js
@@ -1,7 +1,13 @@
 const sum = require('./sum.js');
 
 describe (`Teste se a função realiza as somas corretamente`, () =>{
-    it(`Somar dois valores`, () =>{
+    it(`Teste se o retorno de sum(4, 5) é 9`, () =>{
         expect(sum(4, 5)).toBe(9);
+    })
+    it(`Teste se o retorno de sum(0, 0) é 0`, () =>{
+        expect(sum(0, 0)).toBe(0);
+    })
+    it(`Teste se a função sum lança um erro quando os parâmetros são 4 e "5"(string 5)`, () =>{
+        expect(()=>sum(4, "5")).toThrow();
     })
 })
