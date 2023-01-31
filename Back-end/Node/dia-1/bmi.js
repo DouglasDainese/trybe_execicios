f4a2d1b2baf52c0271ebd7bf17e785579bd8dd8e
DouglasDainese <douglas_dainese@hotmail.com>
2023-01-30 21:52:17 -0300

    Requisito 6 concluído - Calculadora de IMC finalizada

--- Back-end/Node/dia-1/bmi.js
+++ Back-end/Node/dia-1/bmi.js
@@ -1,6 +1,20 @@
 const calculadoraImc = (weight, height) => {
- const result =  (weight / (height * height)).toFixed(2);
- return result;
+ const imc =  (weight / (height * height)).toFixed(2);
+ result = '';
+ if (imc < 18.5) {
+    result =  `Abaixo do peso (magreza)`
+  } else if (imc >= 18.5 && imc <= 24.9) {
+    result =  `Peso normal`
+  } else if (imc >= 25 && imc <= 29.9) {
+    result =  'Acima do peso (sobrepeso)'
+  } else if (imc >= 30 && imc <= 34.9){
+    result =  'Obesidade grau I'
+  } else if (imc >= 35 && imc <= 39.9) {
+    result =  'Obesidade grau II'
+  } else {
+    result =  'Obesidade grau IV e V'
+  };
+  return `Seu IMC é de ${imc} e você está com ${result}`
 };
 
 
