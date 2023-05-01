90d6b34fa2719b07e3cb82b592943992f0f391bb
DouglasDainese <douglas_dainese@hotmail.com>
2023-04-30 23:55:58 -0300

    Exercicio -1, criei minha primeira classe

---
+++ Back-end/POO/Dia-2/entities/Person.ts
@@ -0,0 +1,56 @@
+class Person {
+
+  constructor(
+    private _name: string,
+    private _birthDate: Date,
+  ) {
+    this.validatePerson()
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public get birthDate(): Date {
+    return this._birthDate;
+  }
+
+  public set name(nome: string) {
+    this.validateName(nome);
+    this._name = nome;
+  }
+
+  public set birthDate(data: Date) {
+    this.validateDate(data);
+    this._birthDate = data;
+  }
+
+  private validateName(name: string): void {
+    if (name.length < 3) {
+      throw new Error("O nome deve ter mais de 2 caracteres");
+    }
+  }
+
+  static getAge(date: Date): number {
+    const diff = Math.abs(new Date().getTime() - date.getTime());
+    const yearMs = 31_536_000_000;
+    return Math.floor(diff / yearMs);
+  }
+
+  private validateDate(date: Date): void {
+    if (date.getTime() > new Date().getTime()) {
+      throw new Error("A data não pode ser uma data futura");
+    }
+    if (Person.getAge(date) > 120/*idade maxima*/) {
+      throw new Error("A pessoa não pode possuir mais de 120 anos");
+    }
+  }
+
+  private validatePerson(): void {
+    this.validateDate(this.birthDate);
+    this.validateName(this.name)
+  }
+
+}
+
+export default Person;
---
+++ Back-end/POO/Dia-2/index.ts
@@ -0,0 +1,5 @@
+import Person from "./entities/Person";
+
+const douglas = new Person('Douglas', new Date('1991-04-19'))
+
+console.log(douglas);
